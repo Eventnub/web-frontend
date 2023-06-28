b40456d2c5332f18553ce7412df39005fa642204
ofojichigozie <ofojichigozie@gmail.com>
2023-06-28 10:43:28 +0100

    Added extra field

--- src/components/home/ConditionalPopup.js
+++ src/components/home/ConditionalPopup.js
@@ -1,18 +1,21 @@
 import PropTypes from 'prop-types';
-import { Dialog, DialogContent, TextField, Typography, Box, Stack, Button } from '@mui/material';
+import { Dialog, DialogContent, TextField, Typography, Box, Stack, IconButton } from '@mui/material';
 import * as Yup from 'yup';
 import { Formik, Form, Field } from 'formik';
 import Swal from 'sweetalert2';
 import { LoadingButton } from '@mui/lab';
 import Image from '../Image';
+import Iconify from '../Iconify';
 import WelcomePhoto from '../../assets/welcome.jpg';
 import { requests } from '../../api/requests';
 
 export default function ConditionalPopup({ open, handleClose }) {
   const initialValues = {
+    firstName: '',
     email: '',
   };
   const validationSchema = Yup.object().shape({
+    firstName: Yup.string().required('First name is required'),
     email: Yup.string().email('Invalid email').required('Email is required'),
   });
 
@@ -27,6 +30,11 @@ export default function ConditionalPopup({ open, handleClose }) {
         },
       }}
     >
+      <Stack direction="row" justifyContent="end" sx={{ p: '1rem' }}>
+        <IconButton aria-label="Close" onClick={handleClose}>
+          <Iconify icon="eva:close-fill" width={18} height={18} />
+        </IconButton>
+      </Stack>
       <DialogContent>
         <Box sx={{ display: 'flex', justifyContent: 'center', mb: '2rem' }}>
           <Image alt="Welcome Photo" loading="lazy" src={WelcomePhoto} sx={{ width: '200px' }} />
@@ -60,10 +68,24 @@ export default function ConditionalPopup({ open, handleClose }) {
               }
             }}
           >
-            {({ values, errors, touched, handleChange, isSubmitting }) => (
+            {({ values, errors, touched, handleChange, isSubmitting, isValid, dirty }) => (
               <Form autoComplete="off">
                 <Field
                   as={TextField}
+                  // size="small"
+                  placeholder="First name"
+                  value={values.firstName}
+                  name="firstName"
+                  error={touched.firstName && errors.firstName}
+                  helperText={touched.firstName && errors.firstName}
+                  variant="outlined"
+                  fullWidth
+                  onChange={handleChange}
+                  sx={{ mb: '1rem' }}
+                />
+                <Field
+                  as={TextField}
+                  // size="small"
                   placeholder="Email"
                   value={values.email}
                   name="email"
@@ -77,8 +99,10 @@ export default function ConditionalPopup({ open, handleClose }) {
                 <Stack direction="row" justifyContent="end">
                   <LoadingButton
                     variant="contained"
+                    size="large"
                     type="submit"
                     loading={isSubmitting}
+                    disabled={!(isValid && dirty)}
                     sx={{
                       boxShadow: 'none',
                       backgroundColor: '#1358A5',
@@ -88,19 +112,6 @@ export default function ConditionalPopup({ open, handleClose }) {
                   >
                     Submit
                   </LoadingButton>
-                  <Button
-                    variant="contained"
-                    color="error"
-                    onClick={handleClose}
-                    sx={{
-                      boxShadow: 'none',
-                      borderRadius: '5px',
-                      ml: '0.5rem',
-                      px: '2rem',
-                    }}
-                  >
-                    Close
-                  </Button>
                 </Stack>
               </Form>
             )}
