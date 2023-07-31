59d56b185fff3970b6afa892b03317473bc2afb3
ofojichigozie <ofojichigozie@gmail.com>
2023-07-31 16:58:47 +0100

    Modified 'How it works' UI

--- src/components/home/About.js
+++ src/components/home/About.js
@@ -1,25 +1,41 @@
 import React from 'react';
-import { Container, Box, Typography } from '@mui/material';
+import { Container, Grid, Stack, Typography } from '@mui/material';
+
+const STEPS = [
+  {
+    index: 1,
+    description: 'Find any afrobeat event in your area with your favourite performing artiste',
+  },
+  {
+    index: 2,
+    description: 'Prove how much you know your favourite artiste by playing a game',
+  },
+  {
+    index: 3,
+    description: 'Win the game and get free or discounted ticket to the event.',
+  },
+];
 
 export default function About() {
   return (
     <Container sx={{ marginTop: '4rem', marginBottom: '4rem' }}>
-      <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
-        <Typography variant="h4" sx={{ color: '#000', textAlign: 'center', mb: '2rem' }}>
-          How it Works
-        </Typography>
-        <Box sx={{ width: '100%' }}>
-          <Typography paragraph align="center" sx={{ color: '#000', fontWeight: '200' }}>
-            Find any afrobeat event in your area with your favourite performing artiste
-          </Typography>
-          <Typography paragraph align="center" sx={{ color: '#000', fontWeight: '200' }}>
-            Prove how much you know your favourite artiste by playing a game
-          </Typography>
-          <Typography paragraph align="center" sx={{ color: '#000', fontWeight: '200' }}>
-            Win the game and get free or discounted ticket to the event.
-          </Typography>
-        </Box>
-      </Box>
+      <Typography variant="h4" sx={{ color: '#000', textAlign: 'center', mb: '1rem' }}>
+        How it Works
+      </Typography>
+      <Grid container gap={1}>
+        {STEPS.map((step) => (
+          <Grid key={step.index} Item xs={12}>
+            <Stack direction="column" justifyContent="center" alignItems="center" sx={{ px: '1rem' }}>
+              <Typography variant="subtitle1" sx={{ textAlign: 'center', color: 'grey.500' }}>
+                Step {step.index}.
+              </Typography>
+              <Typography paragraph sx={{ textAlign: 'center', color: '#000', fontWeight: '200', maxWidth: '400px' }}>
+                {step.description}
+              </Typography>
+            </Stack>
+          </Grid>
+        ))}
+      </Grid>
     </Container>
   );
 }
